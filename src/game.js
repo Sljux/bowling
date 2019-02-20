380a6b5d30138fba0371cf9dde6ac86819a9ea71
sljux <aljosa88@gmail.com>
2019-02-20 21:26:06 +0100

    Cleanup

--- src/game.js
+++ src/game.js
@@ -1,22 +1,41 @@
+function isStrike(pins, pinIndex) {
+  return pins[pinIndex] === 10;
+}
+
+function isSpare(frameScore) {
+  return frameScore === 10;
+}
+
+function calculateFrameScore(pins, pinIndex) {
+  return pins[pinIndex] + pins[pinIndex + 1];
+}
+
+function strikeBonus(pins, pinIndex) {
+  return pins[pinIndex + 1] + pins[pinIndex + 2];
+}
+
+function spareBonus(pins, pinIndex) {
+  return pins[pinIndex + 2];
+}
+
 module.exports.calculateScore = function calculateScore(pins) {
   let score    = 0;
   let pinIndex = 0;
 
   for (let frame = 0; frame < 10; frame++) {
-    if (pins[pinIndex] === 10) {
-      score += 10 + pins[pinIndex + 1] + pins[pinIndex + 2];
+    if (isStrike(pins, pinIndex)) {
+      score += 10 + strikeBonus(pins, pinIndex);
       pinIndex++;
-      continue;
+    } else {
+      const frameScore = calculateFrameScore(pins, pinIndex);
+      score += frameScore;
+
+      if (isSpare(frameScore)) {
+        score += spareBonus(pins, pinIndex);
+      }
+
+      pinIndex += 2;
     }
-
-    const frameScore = pins[pinIndex] + pins[pinIndex + 1];
-    score += frameScore;
-
-    if (frameScore === 10) {
-      score += pins[pinIndex + 2];
-    }
-
-    pinIndex += 2;
   }
 
   return score;
